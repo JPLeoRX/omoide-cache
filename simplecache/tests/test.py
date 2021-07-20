33d73b1bb9c7a50fc307407fac321530e747bf24
leo <leo.ertuna@gmail.com>
2021-07-20 03:57:54 +0300

    Implement max size policy

--- simplecache/tests/test.py
+++ simplecache/tests/test.py
@@ -1,5 +1,6 @@
 import time
 import threading
+import operator
 from typing import List, Dict
 
 
@@ -13,7 +14,14 @@ class ExampleService:
         return result
 
 class Cache:
-    def __init__(self, call_to_execute):
+    def __init__(self, call_to_execute, max_allowed_size: int = 2, expire_mode: str = 'ACCESS_COUNT_BASED'):
+        self.expire_mode_update_time_based = 'UPDATE_TIME_BASED'
+        self.expire_mode_access_time_based = 'ACCESS_TIME_BASED'
+        self.expire_mode_access_count_based = 'ACCESS_COUNT_BASED'
+
+        self.max_allowed_size = max_allowed_size # If cache becomes larger than that - results will be removed according to expire mode policy
+        self.expire_mode = expire_mode # ACCESS_BASED (invalidate items that were not updated or accessed)
+        self.expire_duration_ms = 2 * 60 # If the item is older than this - it gets removed
         self.call_to_execute = call_to_execute
 
         # Map that stores the results {key -> result}
@@ -33,6 +41,35 @@ class Cache:
         self.access_counter_map_lock = threading.Lock()
 
 
+    def _assert_max_size(self):
+        while len(self.results_map) > self.max_allowed_size:
+            with self.results_map_lock and self.last_computed_map_lock and self.last_accessed_map_lock and self.access_counter_map_lock:
+                key = self._find_key_to_remove()
+                self.results_map.pop(key)
+                self.last_computed_map.pop(key)
+                self.last_accessed_map.pop(key)
+                self.access_counter_map.pop(key)
+                print('Cache._assert_max_size(): Dropped ' + str(key))
+
+    def _find_key_to_remove(self) -> str:
+        if self.expire_mode == self.expire_mode_access_time_based:
+            return self._find_key_first_accessed()
+        elif self.expire_mode == self.expire_mode_update_time_based:
+            return self._find_key_first_computed()
+        elif self.expire_mode == self.expire_mode_access_count_based:
+            return self._find_key_least_accessed()
+        else:
+            raise RuntimeError('Expire mode ' + str(self.expire_mode) + ' is not implemented yet')
+
+    def _find_key_first_accessed(self):
+        return min(self.last_accessed_map.items(), key=operator.itemgetter(1))[0]
+
+    def _find_key_first_computed(self):
+        return min(self.last_computed_map.items(), key=operator.itemgetter(1))[0]
+
+    def _find_key_least_accessed(self):
+        return min(self.access_counter_map.items(), key=operator.itemgetter(1))[0]
+
     def get(self, positional_arguments: List, keyword_arguments: Dict):
         t1 = time.time()
 
@@ -51,6 +88,9 @@ class Cache:
         # Update all map data
         self._update_in_last_accessed_map(key)
         self._update_in_access_counter_map(key)
+
+        # Track size
+        self._assert_max_size()
 
         t2 = time.time()
         print('Cache.get() With positional_arguments=' + str(positional_arguments) + ', keyword_arguments=' + str(keyword_arguments) + ' took ' + str(round(t2 - t1, 2)) + ' seconds')
@@ -84,11 +124,13 @@ class Cache:
 
 s = ExampleService()
 c = Cache(s.costly_method)
+c.get(['abc', 1], {})
+c.get(['abc', 1], {})
+c.get(['abc', 1], {})
+c.get(['abc', 2], {})
+c.get(['abc', 3], {})
 c.get(['abc', 4], {})
-c.get(['abc', 4], {})
-c.get(['abc', 5], {})
-c.get(['abc', 4], {})
-c.get(['abc', 5], {})
+c.get(['abc', 1], {})
 
 print(c.last_accessed_map)
 print(c.access_counter_map)
