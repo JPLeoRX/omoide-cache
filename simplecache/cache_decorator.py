68a62e1233f76eb6b807b85f840f07cf00c6ee89
leo <leo.ertuna@gmail.com>
2021-07-27 04:22:37 +0300

    Some minor documentation on caches

--- simplecache/cache_decorator.py
+++ simplecache/cache_decorator.py
@@ -1,6 +1,18 @@
 import inspect
 from simplecache.cache import ExpireMode, RefreshMode, Cache
 
+
+# This is a very simple decorator version of the cache. It attached itself to the method, and proxies all requests to the method throught the cache
+# Performance is yet under question, as we rely heavily on reflections to make this all work
+# Basically for each annotated method we create a new cache, and bind it to the method's object instance as a new field
+# Then each call to a method is forwarded into the cache
+
+# All cache creation parameters are kept as decorator arguments, so you can tweak the settings easily
+
+# THIS WILL CRASH ON FUNCTIONS THAT ARE NOT CLASS METHODS!!!!
+
+# TODO, for some weird reason this works only with "@cache_decorator(...)" call, while with no arguments "@cache_decorator" fails
+#  Needs more time and investigation why that happens. If you want to use it without arguments just add "@cache_decorator()", keep empty parantheses
 def cache_decorator(max_allowed_size: int = 100, size_expire_mode: str = ExpireMode.ACCESS_COUNT_BASED,
                     expire_by_computed_duration_s: int = -1, expire_by_access_duration_s: int = -1,
                     refresh_duration_s: int = -1, refresh_mode: str = RefreshMode.COUPLED, refresh_period_s: int = -1,
