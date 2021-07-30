8058d3f312908ca8758136165b36a2b068abfc13
leo <leo.ertuna@gmail.com>
2021-07-30 03:39:26 +0300

    Example that uses both required and optional arguments

---
+++ simplecache/examples/example_calculation/main.py
@@ -0,0 +1,19 @@
+from simplecache.cache_decorator import cache_decorator
+
+
+class CalculationService:
+    @cache_decorator()
+    def weird_method(self, x: float, y: float, z: float = 0) -> float:
+        return x * y + z
+
+
+service = CalculationService()
+
+assert service.weird_method(1, 2) == 2.0
+assert service.weird_method(2, 1) == 2.0
+assert service.weird_method(3, 2) == 6.0
+assert service.weird_method(2, 3) == 6.0
+assert service.weird_method(1, 1, 7) == 8.0
+assert service.weird_method(2, 8, 10) == 26.0
+assert service.weird_method(2, 8, z=10) == 26.0
+assert service.weird_method(20, 2, z=5) == 45.0
